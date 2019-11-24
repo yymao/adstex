yymao/adstex

commit efce158a77faef68dc8e4f0e8359c4bca2644bdc

flatten the logic

adstex.py
@@ -300,29 +300,35 @@ def main():
     all_entries = defaultdict(list)
 
     for key in keys:
-        if key in bib.entries_dict or (
-                args.merge_other and key in bib_other.entries_dict):
-            if args.update:
-                bibcode = extract_bibcode(bib.entries_dict[key])
-                bibcode_new = entry2bibcode(bib.entries_dict[key])
-                if bibcode_new:
-                    all_entries[bibcode_new].append(key)
-                    if bibcode_new != bibcode or args.force_regenerate:
-                        to_retrieve.add(bibcode_new)
-                        print('{}:{} UPDATE => {}'.format(
-                            key,
-                            '' if key in bib.entries_dict else 'FOUND IN SECONDARY BIB SOURCES,',
-                            bibcode_new,
-                        ))
-            elif key in bib.entries_dict:
-                print('{}: EXISTING'.format(key))
-            else:
-                bib.entries_dict[key] = bib_other.entries_dict[key]
-                bib.entries = list(bib.entries_dict.values())
-                print('{}: FOUND IN OTHER BIB SOURCE, MERGED'.format(key))
+        key_exists = (key in bib.entries_dict)
+        key_exists_in_others = (key in bib_other.entries_dict)
+
+        if (key_exists and args.update) or (
+                key_exists_in_others and args.merge_other and args.update):
+            bibcode = extract_bibcode(bib.entries_dict[key])
+            bibcode_new = entry2bibcode(bib.entries_dict[key])
+            if bibcode_new:
+                all_entries[bibcode_new].append(key)
+                if bibcode_new != bibcode or args.force_regenerate:
+                    to_retrieve.add(bibcode_new)
+                    print('{}:{} UPDATE => {}'.format(
+                        key,
+                        '' if key_exists else 'FOUND IN SECONDARY BIB SOURCES,',
+                        bibcode_new,
+                    ))
+                    continue
+
+        if key_exists:
+            print('{}: EXISTING'.format(key))
             continue
 
-        if key in bib_other.entries_dict:
+        if key_exists_in_others and args.merge_other:
+            bib.entries_dict[key] = bib_other.entries_dict[key]
+            bib.entries = list(bib.entries_dict.values())
+            print('{}: FOUND IN OTHER BIB SOURCE, MERGED'.format(key))
+            continue
+
+        if key_exists_in_others:
             print('{}: FOUND IN OTHER BIB SOURCE, IGNORED'.format(key))
             continue
 

commit 5ae7b5fecf574eedec8467ebe15059d5870b792d

Fix bug introduced in efce158a (#14)

adstex.py
@@ -391,11 +391,16 @@ def main():
         key_exists = key in bib.entries_dict
         key_exists_in_others = key in bib_other.entries_dict
 
-        if (key_exists and args.update) or (
-            key_exists_in_others and args.merge_other and args.update
-        ):
-            bibcode = extract_bibcode(bib.entries_dict[key])
-            bibcode_new = entry2bibcode(bib.entries_dict[key])
+        if args.update:
+            if key_exists:
+                bibcode = extract_bibcode(bib.entries_dict[key])
+                bibcode_new = entry2bibcode(bib.entries_dict[key])
+            elif key_exists_in_others and args.merge_other:
+                bibcode = extract_bibcode(bib_other.entries_dict[key])
+                bibcode_new = entry2bibcode(bib_other.entries_dict[key])
+            else:
+                bibcode_new = None
+
             if bibcode_new:
                 all_entries[bibcode_new].append(key)
                 if bibcode_new != bibcode or args.force_regenerate: